f907605b4a0f1c4b21a5fd36e15ffccd26bc0fbd
Gabryel Adriano Borges de Souza <byelacademico@gmail.com>
2023-10-10 22:35:46 -0300

    deleting dhc

--- conversor.py
+++ conversor.py
@@ -1,7 +1,6 @@
 import dash
 from dash import dcc, Input, Output, html, State
 import dash_bootstrap_components as dbc
-import dash_html_components as dhc
 from styles import Styles
 
 def parab10(num, bi):
